8d46e9697b622eb0b16440f852faa0e637860758
Alex Gerasimov <alcordom@gmail.com>
2024-09-15 17:07:23 +0400

    implemented connect and disconnect buttons

--- src/providers/ChainProvider.tsx
+++ src/providers/ChainProvider.tsx
@@ -1,21 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 import { createConfig, http, WagmiProvider } from 'wagmi';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 import { injected } from 'wagmi/connectors';
 
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  connectors: [injected()],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-});
 export const ChainProvider = ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
 
-  console.log('config', config);
+  const config = createConfig({
+    chains: [sepolia],
+    connectors: [injected()],
+    transports: {
+      [sepolia.id]: http(),
+    },
+  });
 
-  return <WagmiProvider config={config}>{children}</WagmiProvider>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
 };
--- src/routing/ProtectedRoute.tsx
+++ src/routing/ProtectedRoute.tsx
@@ -1,12 +1,25 @@
 import { Outlet } from 'react-router-dom';
-import { useAccount } from 'wagmi';
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { injected } from 'wagmi/connectors';
 
 export const ProtectedRoute = () => {
-  const { isConnected } = useAccount();
+  const { isConnected, addresses: address, chain } = useAccount();
+  const { connect } = useConnect();
+  const { disconnect } = useDisconnect();
+  console.log('isConnected', isConnected);
+  console.log('address', address);
+  console.log('chain', chain);
+
+  const handleConnect = () => {
+    if (address) return disconnect();
+
+    connect({ connector: injected() });
+  };
 
   return (
     <>
       <div>ProtectedRoute</div>
+      <button onClick={handleConnect}>{address ? 'Disconnect' : 'Connect'}</button>
       <Outlet />
     </>
   );
