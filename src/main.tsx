54a7275a69ccd828b22bed727f2f992fd4143f68
Alex Gerasimov <alcordom@gmail.com>
2024-09-18 18:13:49 +0400

    started to adjust styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 import { App } from './App.tsx';
 import './styles/main.scss';
@@ -8,6 +8,7 @@ import './styles/main.scss';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,25 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
 
 const theme = extendTheme({
+  config,
   colors: {
     brand: {
       900: '#0b0b0b',
       800: '#25ffa3',
-      600: '#f07300',
-      500: '#0927f1',
-      700: '#dddddd',
+      700: '#f07300',
+      600: '#0927f1',
+      500: '#dddddd',
+      400: '#ffffff',
     },
   },
   fonts: {
-    heading: '"Darker Grotesque", sans-serif',
-    body: '"Darker Grotesque", sans-serif',
+    heading: '"Darker Grotesque", "Avenir", "Helvetica", "Arial", sans-serif',
+    body: '"Darker Grotesque", "Avenir", "Helvetica", "Arial", sans-serif',
   },
 });
 
