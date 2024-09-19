b252597c0bb21e125b11daa559cd1b1ff0e607bf
Alex Gerasimov <alcordom@gmail.com>
2024-09-19 18:04:02 +0400

    added react hook form library and implemented form on the connectors page

--- src/components/Connectors.tsx
+++ src/components/Connectors.tsx
@@ -1,6 +1,7 @@
 import { OpenloginLoginParams } from '@web3auth/openlogin-adapter';
 import { sepolia } from 'viem/chains';
 import { useConnect } from 'wagmi';
+import { useForm } from 'react-hook-form';
 import { Web3AuthConnectorInstance } from '../providers/ChainProvider/Web3AuthConnectorInstance';
 import {
   Box,
@@ -8,6 +9,7 @@ import {
   Divider,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
   Image,
@@ -26,8 +28,14 @@ interface SingleValueFormType {
   readonly emailAddress: string;
 }
 
+// TODO: Tidy up the component.
 export const Connectors = () => {
   const { connect, connectors } = useConnect();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<SingleValueFormType>();
 
   const connectWithWeb3Auth = (options?: OpenloginLoginParams) =>
     connect({ connector: Web3AuthConnectorInstance(sepolia, options) });
@@ -77,12 +85,23 @@ export const Connectors = () => {
         </Button>
       </Flex>
 
-      <form>
-        <FormControl mb="4">
-          <FormLabel>Email</FormLabel>
-          <Input name="emailAddress" placeholder="E.g. hello@example.com" />
+      <form onSubmit={handleSubmit(loginWithEmail)}>
+        <FormControl mb="4" isInvalid={!!errors.emailAddress}>
+          <FormLabel htmlFor="emailAddress">Email</FormLabel>
+          <Input
+            id="emailAddress"
+            placeholder="E.g. hello@example.com"
+            {...register('emailAddress', {
+              required: 'This is required',
+              pattern: {
+                message: 'It should be valid email',
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              },
+            })}
+          />
+          <FormErrorMessage>{errors.emailAddress && errors.emailAddress.message}</FormErrorMessage>
         </FormControl>
-        <Button w="full" colorScheme="green" type="submit">
+        <Button w="full" colorScheme="green" isLoading={isSubmitting} type="submit">
           Continue with Email
         </Button>
       </form>
