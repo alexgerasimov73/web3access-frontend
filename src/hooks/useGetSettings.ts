55a3465669747c273b93ae9a9fc9a2fc11d10825
Alex Gerasimov <alcordom@gmail.com>
2024-12-07 14:54:55 +0400

    added the info toast

--- src/hooks/useGetSettings.ts
+++ src/hooks/useGetSettings.ts
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/react'
 import { useQuery } from '@tanstack/react-query'
 
 import { getSettingsSevice } from '../services/UserService'
@@ -5,10 +6,18 @@ import { useStore } from '../store/useStore'
 
 export const useGetSettings = () => {
 	const setSettings = useStore(state => state.setSettings)
+	const toast = useToast()
 
 	return useQuery({
 		queryKey: ['get settings'],
 		queryFn: async () => {
+			toast({
+				duration: 10_000,
+				title: 'For your information',
+				description:
+					'ATTENTION! The first request to the server might take 2-3 minutes because it launches the server and needs some time to be ready. Sorry!',
+				status: 'info'
+			})
 			const response = await getSettingsSevice()
 			setSettings(response.data)
 
